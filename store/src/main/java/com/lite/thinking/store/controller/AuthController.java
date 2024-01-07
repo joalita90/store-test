1197df600d74f12011e10d36d53650d0ea382dc2
Joally ZG <joally.zuniga@gmail.com>
2024-01-07 12:22:57 -0500

    changes database configuration

---
+++ store/src/main/java/com/lite/thinking/store/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.lite.thinking.store.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
+public class AuthController {
+
+}
---
+++ store/src/main/java/com/lite/thinking/store/service/AuthService.java
@@ -0,0 +1,7 @@
+package com.lite.thinking.store.service;
+
+public interface AuthService {
+	
+	
+
+}
