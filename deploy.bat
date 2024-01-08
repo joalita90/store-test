af85f888eac6d091974bcd4d7099c89e9b457a58
Eisner <eisner271190@gmail.com>
2024-01-07 20:28:49 -0500

    quitar sql server

--- deploy.bat
+++ deploy.bat
@@ -12,8 +12,10 @@ docker build -t ms-store:latest .
 docker run --name ms-store -p 8080:8080 -d ms-store:latest
 docker tag ms-store:latest joallyzg/ms-store:latest
 docker push joallyzg/ms-store:latest
+docker stop ms-store
 docker rm ms-store
 docker rmi ms-store
+docker stop joallyzg/ms-store
 docker rm joallyzg/ms-store
 docker rmi joallyzg/ms-store
 
@@ -26,6 +28,7 @@ docker push joallyzg/app-store:latest
 docker stop app-store
 docker rm app-store
 docker rmi app-store
+docker stop joallyzg/app-store
 docker rm joallyzg/app-store
 docker rmi joallyzg/app-store
 
